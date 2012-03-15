737149854e26a0a7be93cbad828e4af736815d69
Dante Soares <dms3@rice.edu>
2012-03-15 13:50:35 -0500

    Fixed issues in previous pull request, added fix that removes projectless draft questions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < AdminController
   def confirm
     @user = User.find(params[:user_id])
     @user.confirm!
-    redirect_to(users_path)
+    redirect_to(user_path(@user))
   end
   
 end
