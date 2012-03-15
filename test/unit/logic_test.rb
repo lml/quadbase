a2715ecd55d586115310f375230cb4ba314b7c4d
JP Slavinsky <jpslav@gmail.com>
2012-03-15 14:52:56 -0500

    got seedable random code working on the server

--- test/unit/logic_test.rb
+++ test/unit/logic_test.rb
@@ -13,4 +13,19 @@ class LogicTest < ActiveSupport::TestCase
   test "start with letter or underscore" do
     assert_raise(ActiveRecord::RecordInvalid) {Factory.create(:logic, :variables => '9x')}
   end
+  
+  test "run" do
+    ll = Factory.create(:logic, :code => 'x = 3', :variables => 'x')
+    result = ll.run
+    assert_equal 3, result["result"]["x"]
+  end
+  
+  test 'simple_random' do
+    ll = Factory.create(:logic, :code => 'x = Math.random()', :variables => 'x')
+    result_s2_1 = ll.run(2)
+    result_s2_2 = ll.run(2)
+    result_s3_1 = ll.run(3)
+    assert_equal result_s2_1["result"]["x"], result_s2_2["result"]["x"]
+    assert_not_equal result_s3_1["result"]["x"], result_s2_2["result"]["x"]
+  end
 end
