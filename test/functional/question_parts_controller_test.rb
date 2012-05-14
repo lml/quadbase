dadb9f69bfe35d25d5dfa6190aee1ed0539d2db5
Dante Soares <dms3@rice.edu>
2012-05-14 17:43:09 -0500

    Added functional tests for unlocking question parts

--- test/functional/question_parts_controller_test.rb
+++ test/functional/question_parts_controller_test.rb
@@ -33,4 +33,39 @@ class QuestionPartsControllerTest < ActionController::TestCase
     assert_redirected_to edit_question_path(@multipart_question)
   end
 
+  test "should unlock question part" do
+    sign_in @user
+
+    qs = Factory.create(:question_setup, :content => "Something")
+    pq1 = make_simple_question(:published => true, :question_setup => qs)
+    pqp1 = Factory.create(:question_part, :order => 1, :child_question => pq1)
+    multipart_question2 = pqp1.multipart_question
+    multipart_question2.question_setup = qs
+    multipart_question2.save!
+    pq2 = make_simple_question(:published => true, :question_setup => qs)
+    pqp2 = Factory.create(:question_part, :order => 2, :child_question => pq2,
+                                          :multipart_question => multipart_question2)
+    ProjectQuestion.create(:question => multipart_question2,
+                           :project => Project.default_for_user!(@user))
+    multipart_question2.reload
+
+    assert !multipart_question2.setup_is_changeable?
+    assert multipart_question2.child_questions.first.is_published?
+    assert multipart_question2.child_questions.second.is_published?
+
+    put :unlock, :question_part_id => pqp1.id
+    assert multipart_question2.reload
+    assert !multipart_question2.setup_is_changeable?
+    assert !multipart_question2.child_questions.first.is_published?
+    assert multipart_question2.child_questions.second.is_published?
+    assert_redirected_to edit_question_path(multipart_question2)
+
+    put :unlock, :question_part_id => pqp2.id
+    assert multipart_question2.reload
+    assert multipart_question2.setup_is_changeable?
+    assert !multipart_question2.child_questions.first.is_published?
+    assert !multipart_question2.child_questions.second.is_published?
+    assert_redirected_to edit_question_path(multipart_question2)
+  end
+
 end
