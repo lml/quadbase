91d49247a1351bde68df32571b5895e225040c99
JP Slavinsky <jpslav@gmail.com>
2012-05-17 17:33:11 -0500

    longer time for bullring bring up

--- config/initializers/bullring.rb
+++ config/initializers/bullring.rb
@@ -9,6 +9,7 @@ Bullring.configure do |config|
   config.jvm_init_heap_size = '80m'
   config.jvm_max_heap_size = '80m'
   config.jvm_young_heap_size = '40m'
+  config.server_max_bringup_time = 40
 end
 
 # Give Bullring all the library scripts, don't do this until the migration
