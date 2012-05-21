9770932466e86dd14631d3e853d31b96ee3a13a0
Rachel Green <rlgreen91@gmail.com>
2012-05-21 14:38:28 -0400

    Made the parser more flexible in terms of how it accepts inputs, and added a unit test for individual characters, as well as actual questions.  The parser also identifies strings with non-ASCII characters and raises an exception.

--- lib/QTI_parser.rb
+++ lib/QTI_parser.rb
@@ -13,25 +13,6 @@ class QuestionParser < Parslet::Parser
 	end
 
 	#Single character rules
-	rule(:leftbracket)  { str('[') }
-	rule(:rightbracket) { str(']') }
-	rule(:exclamation)  { str('!') }
-	rule(:lessthan)     { str('<') >> space? }
-	rule(:greaterthan)  { str('>') >> space? }
-	rule(:colon)        { str(':') >> space? }
-	rule(:equal)        { str('=') >> space? }
-	rule(:semicolon)    { str(';') >> space? }
-	rule(:lparen)       { str('(') >> space? }
-	rule(:rparen)       { str(')') >> space? }
-	rule(:qmark)        { str('?') >> space? }
-	rule(:underscore)   { str('_') >> space? }
-	rule(:doublequote)  { str('"') >> space? }
-	rule(:ampersand)    { str('&') >> space? }
-	rule(:comma)        { str(',') >> space? }
-	rule(:percent)      { str('%') >> space? }
-	rule(:caret)        { str('^') >> space? }
-	rule(:dash)         { str('-') >> space? }
-
 	rule(:space)        { match('\s').repeat(1) }
 	rule(:space?)       { space.maybe }
 
@@ -42,17 +23,13 @@ class QuestionParser < Parslet::Parser
 	rule(:star)    { match('\*') >> space? }
 	rule(:fslash)  { match('\/') >> space? }
 	rule(:bslash)  { match('\\') >> space? }
+	rule(:any)     { match('.') >> space? }
 
 	#Grammar parts
-	rule(:punc)       { colon | greaterthan | lessthan | equal | 
-		semicolon | lparen | rparen | period | star | fslash | 
-		qmark | underscore | doublequote | ampersand | comma | 
-		percent | caret | dash | bslash }
-	rule(:begin_ques) { lessthan >> exclamation >> leftbracket >> 
-		letters >> leftbracket }
-	rule(:ques)       { (letters | numbers | punc | end_ques ) >> ques.repeat }
-	rule(:end_ques)   { rightbracket >> rightbracket >> greaterthan }
+	rule(:punc) { period | star | fslash | bslash }
+	rule(:ques) { (letters | numbers | punc | any ) >> ques.repeat }
+	
 
-	rule(:expression) { (begin_ques >> ques) | ques }
+	rule(:expression) { ques }
 	root :expression
 end
