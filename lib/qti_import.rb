78796c6f393851e296aeb8f36c52423337451c75
Rachel Green <rlgreen91@gmail.com>
2012-05-27 11:21:01 -0400

    The import class is done.  Everything works and the structure has been set up so that it can easily be used by a controller.

--- lib/qti_import.rb
+++ lib/qti_import.rb
@@ -4,67 +4,30 @@
 require 'open-uri'
 require 'spqr_parser.rb'
 
-module ImportQuestions
-	def openfile(filename)
-		f = File.open(filename)
-		doc = Nokogiri::XML(f)
+class QTImport 
+	attr_reader :filename, :content_type, :parser, :transformer
+
+private
+	def self.add_questions(project,questions)
+		for a in 0..(questions.length-1)
+			project.add_question!(questions[a])
+		end
 	end
 
-	def savefile(document)
-		a = File.open("/home/railsoer/Documents/values.txt",'w') {|f| f.write(document)}		
+	def self.choose_import(content_type)
+		if (content_type == 'SPQR')
+			a = SPQRParser.new
+			b = SPQRTransform.new
+		end
+		return a, b
 	end
 
-	def createproject
+	def self.createproject
 		a = Project.create(:name => 'Import')
 		# a.add_member!(current_user)
 	end
-end
 
-#For the SPQR content, it seems that a series of questions are embedded 
-#within each <item></item> tag.  As such, we search and save only 
-#content within each tag and "discard" the rest.
-module GetContent
-	def iterate_items(document)
-		items = document.xpath('//item')
-	end
-#Why is this such a roundabout way of getting the question? Well, the 
-#questions and answer choices have the same tag, with the answer choices
-#nested more deeply.  I can do a specific search for the path the answer
-#choices are on and get those back.  But, I can't do a more general 
-#search and get only questions back.  Each time it returns the questions
-#and answer choices, in sequence, but this is a problem, especially since
-#the number of answer choices per question varies widely.  Hence the 
-#roundabout method.
-	def get_questions(content,parser,transformer)
-		ques = Array.new
-		ques_id = Array.new
-		questions = Array.new
-		for z in 0..(content.length-1)
-			y = content[z]
-			ques_num  = y.attributes["ident"].value
-			ques_id << ques_num
-			x = y.xpath('//presentation')
-			w = x[0].children.children.children
-			text = w[0].content
-			v = parser.parse(text)
-			ques1 = transformer.apply(v)
-			ques << ques1
-		end		
-		answers = get_answers(content,parser,transformer,ques_num)
-		for a in 0..(ques.length-1)
-			b = Comment.new(:message => ques_id[a])
-			q = SimpleQuestion.new(:content => ques[a])
-			q.comment_thread = b.comment_thread
-			for i in 0..(answers.length-1)
-				q.answer_choices << answers[i]
-			end
-			q.save!
-			questions << q
-		end
-		return questions
-	end
-
-	def get_answers(content,parser,transformer,id_num)
+	def self.get_answers(content,parser,transformer,id_num)
 		#Find only the relevant answer choices, using the question id number
 		answers = Array.new
 		z = content.xpath('//presentation//response_lid//response_label')
@@ -76,7 +39,7 @@ module GetContent
 			end
 		end
 		choices = Array.new
-		credit = get_credit(content,id_num)
+		credit = self.get_credit(content,id_num)
 		for a in 0..(answers.length-1)
 			b = answers[a].children.children.children
 			text = b[0].content
@@ -88,7 +51,7 @@ module GetContent
 		return choices
 	end
 
-	def get_credit(content,id_num)
+	def self.get_credit(content,id_num)
 		numbers = Array.new
 		points = Array.new
 		a = content.xpath('//resprocessing//respcondition')
@@ -105,49 +68,65 @@ module GetContent
 			k = (j.content).to_f
 			points << k
 		end		
-		credits = normalize(points)		
+		credits = self.normalize(points)		
 	end
 
-	def normalize(array)
+#Why is this such a roundabout way of getting the question? Well, the 
+#questions and answer choices have the same tag, with the answer choices
+#nested more deeply.  I can do a specific search for the path the answer
+#choices are on and get those back.  But, I can't do a more general 
+#search and get only questions back.  Each time it returns the questions
+#and answer choices, in sequence, but this is a problem, especially since
+#the number of answer choices per question varies widely.  Hence the 
+#roundabout method.
+	def self.get_questions(content,parser,transformer)
+		ques = Array.new
+		ques_id = Array.new
+		questions = Array.new
+		for z in 0..(content.length-1)
+			y = content[z]
+			ques_num  = y.attributes["ident"].value
+			ques_id << ques_num
+			x = y.xpath('//presentation')
+			w = x[0].children.children.children
+			text = w[0].content
+			v = parser.parse(text)
+			ques1 = transformer.apply(v)
+			ques << ques1
+		end		
+		answers = self.get_answers(content,parser,transformer,ques_num)
+		for a in 0..(ques.length-1)
+			b = Comment.new(:message => ques_id[a])
+			q = SimpleQuestion.new(:content => ques[a])
+			q.comment_thread = b.comment_thread
+			for i in 0..(answers.length-1)
+				q.answer_choices << answers[i]
+			end
+			q.save!
+			questions << q
+		end
+		return questions
+	end
+
+#For the SPQR content, it seems that a series of questions are embedded 
+#within each <item></item> tag.  As such, we search and save only 
+#content within each tag and "discard" the rest.
+	def self.iterate_items(document)
+		items = document.xpath('//item')
+	end
+
+	def self.normalize(array)
 		a = array.max 
 		for z in 0..(array.length-1)
 			array[z] = array[z]/a
 		end
 		return array
 	end
-end
 
-module AddQuestions
-	def add_questions(project,questions)
-		for a in 0..(questions.length-1)
-			project.add_question!(questions[a])
-		end
+	def self.openfile(filename)
+		f = File.open(filename)
+		doc = Nokogiri::XML(f)
 	end
-end
 
-class QTImport 
-	include ImportQuestions
-	include GetContent
-	include AddQuestions
 
-	attr_reader :filename, :content_type, :parser, :transformer
-
-	def initialize(filename, content_type)
-		@filename = filename
-		@content_type = content_type
-		document = openfile(filename)
-		import_project = createproject
-		parser, transformer = choose_import(content_type)
-		content = iterate_items(document)
-		questions = get_questions(content,parser,transformer)
-		add_questions(import_project,questions)
-	end
-private
-	def choose_import(content_type)
-		if (content_type == 'SPQR')
-			a = SPQRParser.new
-			b = SPQRTransform.new
-		end
-		return a, b
-	end
 end
--- test/unit/qti_import_test.rb
+++ test/unit/qti_import_test.rb
@@ -6,31 +6,56 @@ require 'test_helper'
 class QTImportTest < ActiveSupport::TestCase
 	test "1_question" do	
 		assert_nothing_raised do
-			QTImport.new("#{::Rails.root}/test/fixtures/files/spqr1.xml",'SPQR')
+			document = QTImport.openfile("#{::Rails.root}/test/fixtures/files/spqr1.xml")
+			parser, transformer = QTImport.choose_import('SPQR')
+			content = QTImport.iterate_items(document)
+			questions = QTImport.get_questions(content,parser,transformer)
+			project = QTImport.createproject
+			QTImport.add_questions(project,questions)
 		end
 	end
 
 	test "2_question" do		
 		assert_nothing_raised do
-			QTImport.new("#{::Rails.root}/test/fixtures/files/spqr2.xml",'SPQR')
+			document = QTImport.openfile("#{::Rails.root}/test/fixtures/files/spqr2.xml")
+			parser, transformer = QTImport.choose_import('SPQR')
+			content = QTImport.iterate_items(document)
+			questions = QTImport.get_questions(content,parser,transformer)
+			project = QTImport.createproject
+			QTImport.add_questions(project,questions)
 		end
 	end
 
 	test "5_question" do
 		assert_nothing_raised do
-			QTImport.new("#{::Rails.root}/test/fixtures/files/spqr3.xml",'SPQR')
+			document = QTImport.openfile("#{::Rails.root}/test/fixtures/files/spqr3.xml")
+			parser, transformer = QTImport.choose_import('SPQR')
+			content = QTImport.iterate_items(document)
+			questions = QTImport.get_questions(content,parser,transformer)
+			project = QTImport.createproject
+			QTImport.add_questions(project,questions)
 		end
 	end
 
 	test "25_question" do
 		assert_nothing_raised do
-			QTImport.new("#{::Rails.root}/test/fixtures/files/spqr4.xml",'SPQR')
+			document = QTImport.openfile("#{::Rails.root}/test/fixtures/files/spqr4.xml")
+			parser, transformer = QTImport.choose_import('SPQR')
+			content = QTImport.iterate_items(document)
+			questions = QTImport.get_questions(content,parser,transformer)
+			project = QTImport.createproject
+			QTImport.add_questions(project,questions)
 		end
 	end
 
 	test "all_question" do
 		assert_nothing_raised do
-			QTImport.new("#{::Rails.root}/test/fixtures/files/spqr_original.xml",'SPQR')
+			document = QTImport.openfile("#{::Rails.root}/test/fixtures/files/spqr_original.xml")
+			parser, transformer = QTImport.choose_import('SPQR')
+			content = QTImport.iterate_items(document)
+			questions = QTImport.get_questions(content,parser,transformer)
+			project = QTImport.createproject
+			QTImport.add_questions(project,questions)
 		end
 	end
 end
