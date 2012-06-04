61a93f8accce3c3a7e6969703562c9f63de34512
Joel Yin <jy17@rice.edu>
2012-06-04 18:03:06 -0400

    changed code so that the Project model handles the creation of a new project. Thanks again to Dante magic

--- app/models/project.rb
+++ app/models/project.rb
@@ -38,6 +38,7 @@ class Project < ActiveRecord::Base
   
   
   def self.all_for_user(user)
+    Project.default_for_user!(user)
     ProjectMember.all_for_user(user).collect{|wm| wm.project}
   end
   
