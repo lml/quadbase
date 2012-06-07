cfe67b600d5c097f2c09714295b1b8c9bc850f62
Dante Soares <dms3@rice.edu>
2012-06-07 15:03:17 -0500

    Split a long line in 2

--- app/models/question_collaborator.rb
+++ app/models/question_collaborator.rb
@@ -113,7 +113,8 @@ protected
   end  
   
   def assign_position
-    self.position = (QuestionCollaborator.where{question_id == my{question_id}}.maximum('position') || -1) + 1
+    self.position = (QuestionCollaborator.where{question_id == my{question_id}}\
+                                         .maximum('position') || -1) + 1
   end
 
   def no_roles
