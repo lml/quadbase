ffaeb65bfcd4bdf5a174ca18d88695a160376605
jrb6 <joelbaranowski@yahoo.com>
2012-07-11 12:34:54 -0500

    temp

--- app/assets/javascripts/jquery_extensions.js
+++ app/assets/javascripts/jquery_extensions.js
@@ -2,3 +2,9 @@
 // License version 3 or later.  See the COPYRIGHT file for details.
 
 jQuery.fn.exists = function(){return jQuery(this).length>0;}
+
+jQuery.fn.closeOnClickOutside = function(){
+  $('.ui-widget-overlay').live("click",function(){
+    jQuery(this).dialog("close");
+  });
+}
