df29c8512bca39a860d6bce306e67f1f497cafbd
Dante Soares <dms3@rice.edu>
2012-07-20 18:59:07 -0500

    Practice widgets scaffolding

---
+++ test/functional/practice_widgets_controller_test.rb
@@ -0,0 +1,10 @@
+# Copyright 2011-2012 Rice University. Licensed under the Affero General Public 
+# License version 3 or later.  See the COPYRIGHT file for details.
+
+require 'test_helper'
+
+class PracticeWidgetsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
