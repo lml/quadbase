a536beb7ad146d705f906290ed10feb3406a56b7
Rachel Green <rlgreen91@gmail.com>
2012-07-21 02:23:26 -0500

    Deleting unnecessary file

--- db/migrate/20120717032712_remove_image_answer_from_question.rb
+++
@@ -1,9 +0,0 @@
-class RemoveImageAnswerFromQuestion < ActiveRecord::Migration
-  def up
-    remove_column :questions, :image_answer
-  end
-
-  def down
-    add_column :questions, :image_answer, :string
-  end
-end
